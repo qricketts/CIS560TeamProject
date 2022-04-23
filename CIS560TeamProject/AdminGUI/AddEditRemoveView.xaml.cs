cb458a5c8bbff398f5ac277ca1439be65978b141
johnlvelasco <velascoj@ksu.edu>
2022-04-23 16:58:52 -0500

    Started on base AdminGUI that allows the admin to select the function(add, edit, remove) they want to use to interact with a model(category, person, place)

---
+++ CIS560TeamProject/AdminGUI/AddEditRemoveView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdminGUI
+{
+    /// <summary>
+    /// Interaction logic for AddEditRemoveView.xaml
+    /// </summary>
+    public partial class AddEditRemoveView : UserControl
+    {
+        private MainWindow _main; 
+        public AddEditRemoveView(MainWindow main)
+        {
+            InitializeComponent();
+            _main = main;
+            textTitle.Text = "Kiosk Admin Interface";
+        }
+
+        private void FunctionSelected(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            _main.Function = button.Content.ToString();
+            _main.ChangeChild(new ModelSelectionView(_main.Function));
+        }
+    }
+}
---
+++ CIS560TeamProject/AdminGUI/ModelSelectionView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdminGUI
+{
+    /// <summary>
+    /// Interaction logic for ModelSelectionView.xaml
+    /// </summary>
+    public partial class ModelSelectionView : UserControl
+    {
+        private MainWindow TraverseTreeForMainWindow
+        {
+            get
+            {
+                DependencyObject parent = this;
+                do
+                {
+                    parent = LogicalTreeHelper.GetParent(parent);
+                }
+                while (!(parent is null || parent is MainWindow));
+                return (MainWindow)parent;
+            }
+        }
+        public ModelSelectionView(string function)
+        {
+            InitializeComponent();
+            textTitle.Text = "Select an item to " + function.ToLower(); 
+        }
+
+        private void ModelSelected(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            string model = button.Content.ToString();
+            MainWindow main = TraverseTreeForMainWindow;
+            main.ChangeChild(new ModelListView(model));
+        }
+    }
+}
