dede61e5943f07795d5b082a36f7399056da2bab
Reece Miller <reece10@ksu.edu>
2022-04-25 06:36:00 -0500

    Initial commit for DataAccess

---
+++ KioskModels/SqlRatingRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KioskData.KioskModels;
+
+namespace KioskData
+{
+    class SqlRatingRepository
+    {
+    }
+}
