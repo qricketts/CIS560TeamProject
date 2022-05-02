9bc19e2592286084520c0e97f9fae3d346e903ab
johnlvelasco <velascoj@ksu.edu>
2022-05-02 11:23:22 -0500

    Working on procedures

---
+++ KioskModels/SQL/Procedures/Person.CreatePerson.sql
@@ -0,0 +1,14 @@
+﻿create or alter procedure Person.CreatePerson
+	@Name nvarchar(64),
+	@Email nvarchar(64),
+	@Password nvarchar(32), 
+	@CreatedOn datetimeoffset,
+	@UpdatedOn datetimeoffset,
+	@PersonId int output
+as
+
+insert Person.Person([Name], Email, [Password], CreatedOn, UpdatedOn)
+values(@Name, @Email, @Password, @CreatedOn, @UpdatedOn); 
+
+set @PersonId = scope_identity(); 
+go
---
+++ KioskModels/SQL/Procedures/Person.FetchPerson.sql
@@ -0,0 +1,9 @@
+﻿create or alter procedure Person.FetchPerson
+	@PersonId int
+as
+
+select P.PersonId, P.[Name], P.Email, P.[Password], P.
+values(@Name, @Email, @Password); 
+
+set @PersonId = scope_identity(); 
+go
